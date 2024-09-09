3100151ea8a459809d1b36e38992392476fee05c
Adib Muhammad Maros <126374796+Adibmaros@users.noreply.github.com>
2024-09-09 10:46:48 +0700

    Delete backend directory
    ds

--- backend/utils/response.js
+++
@@ -1,9 +0,0 @@
-function OK(res, statusCode, data, message) {
-  res.status(statusCode).json({ isFalse: false, data, message });
-}
-
-function ERR(res, statusCode, message) {
-  res.status(statusCode).json({ isFalse: false, message });
-}
-
-module.exports = { OK, ERR };
